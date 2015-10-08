8487012c806479f277bbcab3c254995f93cbb0b2
Umang Garg <umanggarg28@gmail.com>
2015-10-09 02:23:55 +0530

    Initial commit

--- evenfibonacci.java
+++ evenfibonacci.java
@@ -1,10 +1,10 @@
 class EvenFibonacci{
 public static void main(String []args){
-int a = 0;
-int b = 1;
-int sum = 0;
-	for(int i=1; i<=4000000; i++){
-	int c=a+b;
+double a = 0;
+double b = 1;
+double sum = 0;
+	for(double i=1; i<=100; i++){
+	double c=a+b;
 		if(c%2==0){
 		sum+=c;
 		}
